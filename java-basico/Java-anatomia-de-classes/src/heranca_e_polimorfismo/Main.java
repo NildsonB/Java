492072cb1558fc9820555faa7f9907661337f917
Nildson <nildsonb15@gmail.com>
2025-08-19 15:25:40 -0300

    Update herança e polimorfismo

--- java-basico/Java-anatomia-de-classes/src/heranca_e_polimorfismo/Main.java
+++ java-basico/Java-anatomia-de-classes/src/heranca_e_polimorfismo/Main.java
@@ -1,15 +1,49 @@
 package heranca_e_polimorfismo;
 
+import javax.swing.text.StyleContext;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         Employee manager = new Manager();
+        Employee salesman= new Salesman();
+        printEmployee(manager);
+        printEmployee(salesman);
 
-        manager.setName("João");
-        //manager.setLogin("Joao");
-        //manager.setPassword("123456");
+    }
 
-        System.out.println(manager.getName());
-        //System.out.println(manager.getLogin());
-        //System.out.println(manager.getPassword());
+    public static void printEmployee(Employee employee){
+
+        System.out.printf("=======%s=======\n", employee.getClass());
+        switch (employee){
+            case Manager manager -> {
+                manager.setCode("123");
+                manager.setName("João");
+                manager.setSalary(5000);
+                manager.setLogin("Joao");
+                manager.setPassword("123456");
+                manager.setCommision(1200);
+
+                System.out.println(manager.getCode());
+                System.out.println(manager.getName());
+                System.out.println(manager.getSalary());
+                System.out.println(manager.getLogin());
+                System.out.println(manager.getPassword());
+                System.out.println(manager.getCommision());
+            }
+            case Salesman salesman -> {
+                salesman.setCode("456");
+                salesman.setName("Lucas");
+                salesman.setSalary(2800);
+                salesman.setPercetPerSold(10);
+
+
+                System.out.println(salesman.getCode());
+                System.out.println(salesman.getName());
+                System.out.println(salesman.getSalary());
+                System.out.println(salesman.getPercetPerSold());
+            }
+        }
+        System.out.println("==================");
     }
 }
